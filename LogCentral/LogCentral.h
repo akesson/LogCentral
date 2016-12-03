1eeb3e2e98944457e0c3040c0a1939b17c6b802c
Henrik <henrik@akesson.mobi>
2016-12-03 15:54:48 +0100

    Base project

---
+++ LogCentral/LogCentral.h
@@ -0,0 +1,19 @@
+//
+//  LogCentral.h
+//  LogCentral
+//
+//  Created by Henrik Akesson on 3/12/16.
+//  Copyright © 2016 Henrik Akesson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LogCentral.
+FOUNDATION_EXPORT double LogCentralVersionNumber;
+
+//! Project version string for LogCentral.
+FOUNDATION_EXPORT const unsigned char LogCentralVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LogCentral/PublicHeader.h>
+
+
