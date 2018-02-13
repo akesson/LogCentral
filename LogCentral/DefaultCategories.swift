691053c2233fbe306e304b8d4a1b72bb4c1fc881
Henrik Akesson <hakesson@holvi.com>
2018-02-13 19:32:15 +0100

    Changing casing of categories to conform to swift standard

--- LogCentral/DefaultCategories.swift
+++ LogCentral/DefaultCategories.swift
@@ -10,15 +10,28 @@ import Foundation
 
 
 public enum MVVMS: Int, CategorySpec {
-    case View, ViewModel, Model, Service
+    case view, viewModel, model, service
     
-    public static let asArray:[MVVMS] = [.View, .ViewModel, .Model, .Service]
+    public static let asArray:[MVVMS] = [.view, .viewModel, .model, .service]
+}
+
+public enum MVVM: Int, CategorySpec {
+    case view, viewModel, model
+    
+    public static let asArray:[MVVM] = [.view, .viewModel, .model]
 }
 
 
 public enum MVCS: Int, CategorySpec {
-    case View, Model, Controller, Service
+    case view, model, controller, service
     
-    public static let asArray:[MVCS] = [.View, .Model, .Controller, .Service]
+    public static let asArray:[MVCS] = [.view, .model, .controller, .service]
+    
+}
+
+public enum MVC: Int, CategorySpec {
+    case view, model, controller
+    
+    public static let asArray:[MVC] = [.view, .model, .controller]
     
 }
