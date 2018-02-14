76f1560e76b9fea7de7078c90c5c0a33d5b0b80e
Henrik Akesson <hakesson@holvi.com>
2018-02-14 14:18:41 +0100

    Adding comments to the DefaultCategories

--- LogCentral/DefaultCategories.swift
+++ LogCentral/DefaultCategories.swift
@@ -10,28 +10,51 @@ import Foundation
 
 
 public enum MVVMS: Int, CategorySpec {
-    case view, viewModel, model, service
+    /// For logging made in the view (in MVVMS)
+    case view
+    /// For logging made in the viewModel (in MVVMS)
+    case viewModel
+    /// For logging made in the model (in MVVMS)
+    case model
+    /// For logging made in the service (in MVVMS)
+    case service
     
     public static let asArray:[MVVMS] = [.view, .viewModel, .model, .service]
 }
 
 public enum MVVM: Int, CategorySpec {
-    case view, viewModel, model
+    /// For logging made in the view (in MVVM)
+    case view
+    /// For logging made in the viewModel (in MVVM)
+    case viewModel
+    /// For logging made in the model (in MVVM)
+    case model
     
     public static let asArray:[MVVM] = [.view, .viewModel, .model]
 }
 
 
 public enum MVCS: Int, CategorySpec {
-    case view, model, controller, service
+    /// For logging made in the view (in MVCS)
+    case view
+    /// For logging made in the model (in MVCS)
+    case model
+    /// For logging made in the controller (in MVCS)
+    case controller
+    /// For logging made in the service (in MVCS)
+    case service
     
     public static let asArray:[MVCS] = [.view, .model, .controller, .service]
     
 }
 
 public enum MVC: Int, CategorySpec {
-    case view, model, controller
+    /// For logging made in the view (in MVC)
+    case view
+    /// For logging made in the model (in MVC)
+    case model
+    /// For logging made in the controller (in MVC)
+    case controller
     
     public static let asArray:[MVC] = [.view, .model, .controller]
-    
 }
