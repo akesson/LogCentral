549f1efb520bf435797951d151914ecc613e837b
Henrik Akesson <hakesson@holvi.com>
2018-04-26 17:48:26 +0200

    Move LogWriter to main project

--- LogCentral/LogWriter.swift
+++ LogCentral/LogWriter.swift
@@ -1,20 +1,21 @@
 //
-//  ExampleLogWriter.swift
-//  LogCentralTests
+//  LogWriter.swift
+//  LogCentral
 //
 //  Created by Henrik Akesson on 15/02/2018.
 //  Copyright © 2018 Henrik Akesson. All rights reserved.
 //
 
-import Foundation
-import LogCentral
-
 public struct LogWriter: LoggerSpec {
+    
     public let levels: [LogLevel]
     public let messageWriter: LogMessageWriter
     public let errorObjectWriter: LogErrorObjectWriter?
     
-    public init(levels: [LogLevel], messageWriter: @escaping LogMessageWriter, errorObjectWriter: LogErrorObjectWriter?) {
+    public init(levels: [LogLevel],
+                messageWriter: @escaping LogMessageWriter,
+                errorObjectWriter: LogErrorObjectWriter?) {
+        
         self.levels = levels
         self.messageWriter = messageWriter
         self.errorObjectWriter = errorObjectWriter
